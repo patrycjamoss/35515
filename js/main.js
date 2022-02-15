49ebeca3fe0f4b06807ea0fd5ebc25d718180cfa
patrycjamoss <patrycjamoss@gmail.com>
2022-02-15 10:13:05 +0100

    only else

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 let number = 10;
-if (number>10){
+if (number>10)
+{
     console.log('Podana liczba  jest wieksza niz 10')
 }
-else if(number<10){
+else (number<10)
+{
     console.log('Podana liczba nie jest wieksza od 10')
 }
